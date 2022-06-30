5387fa5e628d43dd04ab46d5c3f8aef3398cbcc5
tiahna22 <103657057+tiahna22@users.noreply.github.com>
2022-06-29 22:32:07 -0600

    first commit

---
+++ build/precache-manifest.ac51e6e6c9c410d1741beccdbbd7fd86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ba379d63d0cc275bdc39d11ba47b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4773f73ecceffd57123d",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4773f73ecceffd57123d",
+    "url": "/static/js/main.39a994af.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
